5641f2f4be90a57c80bd597f0b1180eb883d870a
rakhiradhe <60418560+rakhiradhe@users.noreply.github.com>
2021-08-30 19:24:33 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,28 +1,8 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "=2.72.0"
-    }
-  }
-}
-
 provider "azurerm" {
-  features {}
-}
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+  version = "=1.36.0"
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "storageaccountnameq"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-
-  tags = {
-    environment = "staging"
-  }
+resource "azurerm_resource_group" "default" {
+  name     = "${var.name}-${var.environment}-rg"
+  location = "${var.location}"
 }
